8025f316b2a6c4fb00a4528366ca9a0f207d1334
gitusercz <czibere.a@gmail.com>
2022-12-23 21:36:52 +0100

    Wanted to use an ini handler, that was only python3 compatible.
    So BWC was rewritten.

--- BWC.py
+++ BWC.py
@@ -3,20 +3,24 @@
 from datetime import datetime
 from time import strftime
 from shutil import copyfile
-import datetime, commands, re
+import datetime, re
 import time, os.path
 import os
 
 #========================= Constants definitions
-image_frequency=0
-logfile_path = '/home/pi/dev/BWC/BWC_log.txt'
-image_target = '/var/www/html/image.jpg'
-image_target_arch_folder = '/home/pi/dev/BWC/IMG'
-image_target_arch_path = ''
-webpage_path = '/var/www/html/index.html'
-logfile_path = '/var/www/html/log.txt'
+config_file = '/home/pi/dev/BWC_pyth3/config.ini'
 current_timestamp = ''
+image_target_collection_path = ''
 image_cntr=1
+
+#========================= Constants load from config
+from configobj import ConfigObj
+config = ConfigObj(config_file)
+
+logfile_path = config['Paths']['logfile']
+image_target_for_webpage = config['Paths']['image_target_for_webpage']
+image_target_collection_folder = config['Paths']['image_target_collection_folder']
+webpage_path = config['Paths']['webpage_path']
 
 #========================= function definitions 
 def log_to_file(func_logfile_path,func_content):
@@ -33,9 +37,9 @@ log_to_file(logfile_path,"Application started")
 #time.sleep(30)
 
 while True: 
-	os.system("libcamera-jpeg -o " + image_target + " -n --width 800 --height 600")
-	image_target_arch_path = image_target_arch_folder + "/" + '{0:09}'.format(image_cntr)+".jpg"
-	copyfile(image_target,image_target_arch_path)
+	os.system("libcamera-jpeg -o " + image_target_for_webpage + " -n --width 800 --height 600")
+	image_target_collection_path = image_target_collection_folder + "/" + '{0:09}'.format(image_cntr)+".jpg"
+	copyfile(image_target_for_webpage,image_target_collection_path)
 	image_cntr = image_cntr + 1
 	with open(webpage_path,"w") as webpage:
 				#logfile.write(strftime("%Y-%m-%d %H:%M:%S")+",Started!\n")
